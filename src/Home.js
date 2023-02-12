e1a10a63d919809b8c88b4361670212d953694bf
Ali Husnain <pkihusnain220@gmail.com>
2023-02-13 00:02:56 +0500

    Add Loading Message

--- src/Home.js
+++ src/Home.js
@@ -2,30 +2,29 @@ import { useState , useEffect } from "react";
 import BlogList from "./BlogList"
 const Home = () => {
 
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'ali', id: 3 }
-    ])
-
+    const [blogs, setBlogs] = useState(null)
+    const [isPanding, setIsPanding] = useState(true)
     const handleDelete = (id) =>{
         const newBlog = blogs.filter((item) => item.id !== id);
         setBlogs(newBlog);
     }
     useEffect(()=>{
-        fetch('http://localhost:8000/blogs')
+        setTimeout( ()=>{
+            fetch('http://localhost:8000/blogs')
         .then((result) => {
             return result.json();
         })
         .then((data)=>{
-            // console.log(data);
+            setIsPanding(false);
             setBlogs(data);
         })
+        },1000)
 
     } ,[])
     return (
 
         <div className="home">
+            {isPanding && <div>Loading...</div> }
             {blogs && <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>}
         </div>
     );
