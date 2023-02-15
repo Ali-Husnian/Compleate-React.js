293a9e85560512521a6366cf6f17a353b5d07817
Ali Husnain <pkihusnain220@gmail.com>
2023-02-15 23:44:11 +0500

    Deleting post Request

--- src/BlogsDetale.js
+++ src/BlogsDetale.js
@@ -1,8 +1,18 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 const BlogsDetale = () => {
     const { id } = useParams()
     const { data: blog, isPanding, error } = useFetch("http://localhost:8000/blogs/" + id);
+
+    const history = useHistory();
+
+    const handleDelete = ()=>{
+        fetch('http://localhost:8000/blogs/'+blog.id,{
+            method:"DELETE"
+        }).then( () =>{
+            history.push('/')
+        })
+    }
     return (
         <div className="blog-details">
             {isPanding && <div>Loading...</div>}
@@ -12,6 +22,8 @@ const BlogsDetale = () => {
                     <h3>{blog.title}</h3>
                     <p>Writen By {blog.author}</p>
                     <div>{blog.body}</div>
+                    <button onClick={handleDelete}>Delete</button>
+                    <button className="btn-Edit">Edit</button>
                 </article>
             )}
         </div>
