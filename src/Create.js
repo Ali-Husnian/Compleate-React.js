b62935f45fbc2d0c2f00c0321ca8cfa09f26df6f
Ali Husnain <pkihusnain220@gmail.com>
2023-02-15 16:29:38 +0500

    Create Blog Post form

--- src/Create.js
+++ src/Create.js
@@ -1,9 +1,27 @@
-const create = () => {
-    return ( 
-        <div>
-            <h3>Create New Blog</h3>
+import { useState } from "react";
+
+const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('');
+
+    return (
+        <div className="create">
+            <h3>Add a New Blog</h3>
+            <form>
+                <label>Blog title:</label>
+                <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)} />
+                <label>Blog body:</label>
+                <textarea required value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+                <label>Blog author:</label>
+                <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                <button>Add Blog</button>
+            </form>
         </div>
-     );
-}
- 
-export default create;
+    );
+};
+
+export default Create;
